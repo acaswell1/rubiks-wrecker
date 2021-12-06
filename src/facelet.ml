b7f3571986d79fa1ec40296cb2ac41329dcc57fa
Brandon Stride <brandonzstride@hotmail.com>
2021-12-06 14:03:13 -0500

    turn and facelet to_string

--- src/facelet.ml
+++ src/facelet.ml
@@ -13,3 +13,5 @@ let all_facelets = [R1; R2; R3; R4; R5; R6; R7; R8; R9;
                     F1; F2; F3; F4; F5; F6; F7; F8; F9;
                     U1; U2; U3; U4; U5; U6; U7; U8; U9;
                     D1; D2; D3; D4; D5; D6; D7; D8; D9]
+
+let to_string (facelet: t) = facelet |> sexp_of_t |> Core.Sexp.to_string
